86a49aa12460bf93a9f2126ea8e8c028b0543994
namnteiga <n.manteiga@udc.es>
2026-02-18 19:01:55 +0100

    Añadidos sprites al path

---
+++ assets/graphics/environments/sprites cocina/counters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="counters" tilewidth="36" tileheight="36" tilecount="400" columns="20">
+ <image source="counters.jpeg" width="736" height="736"/>
+</tileset>
---
+++ assets/graphics/environments/sprites cocina/floor_blue_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="floor_blue_tiles" tilewidth="93" tileheight="93" tilecount="49" columns="7">
+ <image source="floor_blue_tiles.jpeg" width="736" height="736"/>
+</tileset>
