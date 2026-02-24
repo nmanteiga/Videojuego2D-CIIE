39befe78413297c5fa27d3fd1a26284f695bc798
Antón <a.clopez@udc.es>
2026-02-24 11:50:43 +0100

    sprites 2

---
+++ assets/graphics/environments/sprites cocina/Habitacion.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Habitacion" tilewidth="16" tileheight="16" tilecount="391" columns="17">
+ <image source="../Modern_Interiors_Free_v2.2/Modern tiles_Free/Interiors_free/16x16/Room_Builder_free_16x16.png" trans="ff00ff" width="272" height="368"/>
+</tileset>
